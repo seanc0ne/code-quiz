e90ecda1e215eb17c41019d585ed1015c48617a4
Sean Cone <seandoesitbetter@gmail.com>
2020-06-11 10:53:59 -0700

    Basic quiz functional, saving work

--- js/script.js
+++ js/script.js
@@ -7,9 +7,12 @@ const answerButtonsElement = document.getElementById('answer-buttons')
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame() {
-    console.log('Started')
     startButton.classList.add('hide')
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
@@ -37,15 +40,40 @@ function showQuestion(question) {
 }
 
 function resetState() {
+    clearStatusClass(document.body)
     nextButton.classList.add('hide')
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild(answerButtonsElement.firstChild)
     }
 }
 
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+}
 
-function selectAnswer() {
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
 
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 }
 
 const questions = [
@@ -53,7 +81,19 @@ const questions = [
         question: 'What is 2 + 2?',
         answers: [
             { text: '4', correct: true },
-            { text: '22', correct: false }
+            { text: '22', correct: false },
+            { text: '202', correct: false },
+            { text: '2002', correct: false }
         ]
-    }
+    },
+    {
+        question: 'If 4(x) = 8, what does x equal?',
+        answers: [
+            { text: '4', correct: false },
+            { text: '22', correct: false },
+            { text: '202', correct: false },
+            { text: '2', correct: true }
+        ]
+    },
+    
 ]
