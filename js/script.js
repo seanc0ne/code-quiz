91522d8664092eb57a80732b2fce98e28276c04c
Sean Cone <seandoesitbetter@gmail.com>
2020-06-11 10:28:51 -0700

    Adding work on functions

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
@@ -17,13 +18,31 @@ function startGame() {
 }
 
 function setNextQuestion() {
+    resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
     questionElement.innerText = question.question
-
+    question.answers.forEach(answer => {
+        const button = document.createElement ('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
 }
+
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+
 
 function selectAnswer() {
 
@@ -31,7 +50,7 @@ function selectAnswer() {
 
 const questions = [
     {
-        question: 'What is 2 + 2',
+        question: 'What is 2 + 2?',
         answers: [
             { text: '4', correct: true },
             { text: '22', correct: false }
